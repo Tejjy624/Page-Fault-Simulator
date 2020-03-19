d7d1b272d19c1627581607e53b56cfc9095676b3
Tejjy624 <54224255+Tejjy624@users.noreply.github.com>
2020-03-18 20:06:46 -0700

    Update pfsim.c

--- pfsim.c
+++ pfsim.c
@@ -3,29 +3,77 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 
+#define MAX_SIZE 0xffff 
+
+/* Page table entry */
+typedef struct{
+    bool        valid;
+    uint16_t    pfn;
+}pte;
+
+/* Reverse mapping entry */
+typedef struct{
+    bool        unavail;
+    uint8_t     proc;
+    uint16_t    vpn;
+}rme;
 
 int main(int argc, char *argv[]){
     FILE *fp;
     char *filename;
-    char ch;
+    char *line = NULL;
+    size_t len = 0;
+    int read;
+    char pid[2];
+    char hex[7];
+    char TYPE[2];
+    int PID;
+    int VPN;
+    int disk_access;
+    //int faults;
+    //int page_access;
+    pte PageTable[MAX_SIZE];
 
+
+    //check if file arguement is passed
     if (argc < 2){
-        printf("Usage: ref_pfsim <file>");
-        return 1;
+        printf("Usage: ref_pfsim <file>\n");
+        return(1);
     } else{
         filename = argv[1];
-        printf("Filename: %s\n", filename);
     }
+    //open file
     fp = fopen(filename, "r");
-    if (fp){
-        printf("File contents:\n");
-        while ((ch = fgetc(fp)) != EOF){
-            printf("%c", ch);
+    while (1){
+        while((read = getline(&line, &len, fp)) != -1){
+            //Read line into 3 variables
+            strncpy(pid, line, 1);
+            pid[2] = '\0';
+            PID = (int)strtol(pid, NULL, 0);
+            strncpy(hex, line + 2, 6);
+            hex[7] = '\0';
+            VPN = (int)strtol(hex, NULL, 0);
+            strncpy(TYPE, line + 9, 1);
+            TYPE[2] = '\0';
+            if (PID == 0){
+                printf("Found");
+                PageTable[VPN];
+            }
+            //Create page table entry for VPN
+            
+
+            printf("%d", PID);
+            printf("%d", VPN);
+            //printf("%s\n", TYPE);
+            //printf("%s\n", line);
+            disk_access++;
         }
-    } else{
-        printf("Failed to open the file\n");
+        break;
     }
-    return 0;
+    fclose(fp);
+    //printf("\nPage accesses: %d", page_access);
+    //printf("\nPage Faults: %d", faults);
+    //printf("\nDisk accesses: %d", disk_access);
 }
-    
